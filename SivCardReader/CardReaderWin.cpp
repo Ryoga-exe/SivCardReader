59eaf8e00af9f09caaa425e083cd9ee351635bb5
Ryoga-exe <git@ryoga.dev>
2025-12-12 01:19:26 +0900

    wip

---
+++ SivCardReader/CardReaderWin.cpp
@@ -0,0 +1,1 @@
+﻿# include "CardReaderWin.hpp"
---
+++ SivCardReader/CardReaderWin.hpp
@@ -0,0 +1,26 @@
+﻿# pragma once
+# include <Siv3D.hpp>
+
+class CardReaderWin
+{
+public:
+	using IDm = std::array<uint8, 8>;
+
+	CardReaderWin() = default;
+
+	~CardReaderWin();
+
+	void startScan();
+
+	void stopScan();
+
+	bool isReady() const;
+
+	bool isOK() const;
+
+	IDm getIDm() const;
+
+private:
+	IDm m_idm = {};
+	AsyncTask<void> m_task;
+};
